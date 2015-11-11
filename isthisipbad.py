f63adfe0a0024ab287bb71b511400cd733957e87
Sliim <sliim@mailoo.org>
2015-11-11 01:15:36 +0100

    Print message on nameservers error

--- isthisipbad.py
+++ isthisipbad.py
@@ -220,6 +220,9 @@ if __name__ == "__main__":
         except dns.resolver.Timeout:
             print 'Timeout querying ' + bl
 
+        except dns.resolver.NoNameservers:
+            print 'Nameservers error when querying ' + bl
+
 
 #This Doesnt work because I am stupid.
 print('\n')
